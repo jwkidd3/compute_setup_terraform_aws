9650d3537db96d74a99b7662b02397eab72d594f
jwkidd3 <jkidd@kiddcorp.com>
2022-12-08 18:00:08 -0600

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# compute_setup_terraform
---
+++ ec2_instances.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  count=4
+  key_name="kiddcorp"
+  tags = {
+    Name = "user25-instance-${count.index}",
+    role=count.index==0?"lb": (count.index<3?"web":"backend")
+  }
+}
+
+output "ips"{
+  value = aws_instance.instance.*.public_ip
+}
