50c9bac3c7de0985c95ead3ddbd3119bff79d879
jwkidd3 <jkidd@kiddcorp.com>
2025-04-28 09:23:46 -0500

    more

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,25 +1,25 @@
 terraform {
-backend "s3" {
-    bucket         = "kiddcorptf"
-    key            = "tf/${var.keyvalue}"
-    region         = "us-west-1"
-  }
+  # backend "s3" {
+  #    bucket         = "kiddcorptf"
+  #    region         = "us-west-1"
+  #  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
-    } 
+    }
   }
 }
-variable "keyvalue"{
-  type=string
-  default="tf"
+
+provider "aws" {
+  region = "us-east-1"
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::714223118628:role/automation"
+  #   session_name = "terraform-session"
+  # }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-west-1"
-}
+data "aws_organizations_organization" "org" {}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -36,18 +36,31 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+# resource "aws_key_pair" "key" {
+#   key_name   = "my-ec2-keypair"
+#   public_key = file("~/.ssh/id_rsa.pub") # Replace with your public key path
+# }
 
-resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name="kiddcorp"
-  count=3
-  tags = {
-    Name = "user25-instance-${count.index}",
-    role=count.index==0?"user25-lb": (count.index<3?"user25-web":"user25-backend")
-  }
+#  resource "aws_instance" "instance" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t3.micro"
+#   key_name = aws_key_pair.key.key_name
+#   count=1
+#   tags = {
+#     Name = "user25-instance-${count.index}",
+#     role=count.index==0?"user25-lb": (count.index<3?"user25-web":"user25-backend")
+#   }
+# }
+
+# output "ips"{
+#  value = aws_instance.instance.*.public_ip
+# }
+
+locals {
+  ids = data.aws_organizations_organization.org.accounts[*].id
 }
 
-output "ips"{
-  value = aws_instance.instance.*.public_ip
+# Output the list of account IDs
+output "account_ids" {
+  value = local.ids
 }
